ee9376b8b0fc1c0567c63171b100dddd8d50843b
Eamonn O'Brien <eamonnob@gmail.com>
2021-12-06 22:40:21 +0000

    Day 6 solved.

---
+++ day6.exs
@@ -0,0 +1,25 @@
+fish =
+  File.read!("day/6/input")
+  |> String.trim()
+  |> String.split(",")
+  |> Enum.map(&String.to_integer/1)
+
+days = [80, 256]
+
+Enum.reduce(1..Enum.max(days), Enum.frequencies(fish), fn day, fish ->
+  fish =
+    if Map.has_key?(fish, 0) do
+      fish
+      |> Map.merge(%{9 => Map.get(fish, 0), 7 => Map.get(fish, 7, 0) + Map.get(fish, 0)})
+      |> Map.delete(0)
+    else
+      fish
+    end
+    |> Enum.into(%{}, fn {n, v} -> {n - 1, v} end)
+
+  if day in days do
+    IO.puts("After #{day} days #{Map.values(fish) |> Enum.sum()} fish.")
+  end
+
+  fish
+end)
