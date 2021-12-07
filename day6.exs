d4e59b3b3c8731f4ca93d3ce2f1d3abddad9d434
Eamonn O'Brien <eamonnob@gmail.com>
2021-12-07 03:19:33 +0000

    Removed an unneeded Enum.into for almost no gain.

--- day6.exs
+++ day6.exs
@@ -8,14 +8,13 @@ days = [80, 256]
 
 Enum.reduce(1..Enum.max(days), Enum.frequencies(fish), fn day, fish ->
   fish =
-    if Map.has_key?(fish, 0) do
+    if Map.has_key?(fish, day - 1) do
       fish
-      |> Map.merge(%{9 => Map.get(fish, 0), 7 => Map.get(fish, 7, 0) + Map.get(fish, 0)})
-      |> Map.delete(0)
+      |> Map.merge(%{day + 8 => Map.get(fish, day - 1), day + 6 => Map.get(fish, day + 6, 0) + Map.get(fish, day - 1)})
+      |> Map.delete(day - 1)
     else
       fish
     end
-    |> Enum.into(%{}, fn {days_to_go, fish_spawning} -> {days_to_go - 1, fish_spawning} end)
 
   if day in days do
     IO.puts("After #{day} days #{Map.values(fish) |> Enum.sum()} fish.")
