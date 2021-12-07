1a388ff03c1b6c8e03933e591cb591c3d802ef04
Eamonn O'Brien <eamonnob@gmail.com>
2021-12-07 14:26:07 +0000

    Formatting.

--- day7.exs
+++ day7.exs
@@ -6,7 +6,7 @@ input =
 
 for fuel_calculation <- [& &1, &div((&1 + 1) * &1, 2)] do
   Enum.map(Enum.min(input)..Enum.max(input), fn pos ->
-    Enum.map(input, &(fuel_calculation.(Kernel.abs(&1 - pos))))
+    Enum.map(input, &fuel_calculation.(Kernel.abs(&1 - pos)))
     |> Enum.sum()
   end)
   |> Enum.min()
