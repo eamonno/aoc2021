bb8efb2c420f79bcbdc9d7075a91618c18ca427a
Eamonn O'Brien <eamonnob@gmail.com>
2021-12-07 03:43:11 +0000

    More refactoring.

--- day6.exs
+++ day6.exs
@@ -4,21 +4,14 @@ fish =
   |> String.split(",")
   |> Enum.map(&String.to_integer/1)
 
-days = [80, 256]
-
-Enum.reduce(1..Enum.max(days), Enum.frequencies(fish), fn day, fish ->
-  fish =
-    if Map.has_key?(fish, day - 1) do
-      fish
-      |> Map.merge(%{day + 8 => Map.get(fish, day - 1), day + 6 => Map.get(fish, day + 6, 0) + Map.get(fish, day - 1)})
-      |> Map.delete(day - 1)
-    else
-      fish
+fish_on_day = fn fish, day ->
+  Enum.reduce(1..day, Enum.frequencies(fish), fn day, fish ->
+    case Map.get(fish, day - 1) do
+      nil -> fish
+      n -> fish |> Map.merge(%{day + 8 => n, day + 6 => Map.get(fish, day + 6, 0) + n}) |> Map.delete(day - 1)
     end
+  end) |> Map.values() |> Enum.sum()
+end
 
-  if day in days do
-    IO.puts("After #{day} days #{Map.values(fish) |> Enum.sum()} fish.")
-  end
-
-  fish
-end)
+IO.puts("After  80 days #{fish_on_day.(fish, 80)} fish.")
+IO.puts("After 256 days #{fish_on_day.(fish, 256)} fish.")
