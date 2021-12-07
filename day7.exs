ea6a934266f9c2755c57c7e971e1c8c7439c365c
Eamonn O'Brien <eamonnob@gmail.com>
2021-12-07 13:13:02 +0000

    Day 7 solved.

---
+++ day7.exs
@@ -0,0 +1,14 @@
+input =
+  File.read!("day/7/input")
+  |> String.trim()
+  |> String.split(",")
+  |> Enum.map(&String.to_integer/1)
+
+for fuel_calculation <- [& &1, &div((&1 + 1) * &1, 2)] do
+  Enum.map(Enum.min(input)..Enum.max(input), fn pos ->
+    Enum.map(input, &(fuel_calculation.(Kernel.abs(&1 - pos))))
+    |> Enum.sum()
+  end)
+  |> Enum.min()
+  |> IO.inspect()
+end
